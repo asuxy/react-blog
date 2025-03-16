81c8199bebccce6e281ef66b0abb29e9f41a55fc
asuxy <petkov.k.petko@gmail.com>
2025-03-16 09:01:48 +0200

    Add postgradeSQL to project

---
+++ index.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client'
+import dotenv from 'dotenv';
+dotenv.config();
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const post = await prisma.post.update({
+    where: { id: 1 },
+    data: { published: true },
+  })
+  console.log(post)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
